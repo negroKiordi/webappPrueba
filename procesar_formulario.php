e70ded984560f2c972af6c926eab3827b6ff7615
Alejandro Giordana <96189433+negroKiordi@users.noreply.github.com>
2024-05-30 20:40:10 -0300

    Create procesar_formulario.php

---
+++ procesar_formulario.php
@@ -0,0 +1,32 @@
+<?php
+// Datos de conexión a la base de datos
+$servername = "localhost";
+$username = "tu_usuario";
+$password = "tu_contraseña";
+$dbname = "tu_base_de_datos";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Comprobar conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+// Recibir los datos del formulario
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$mensaje = $_POST['mensaje'];
+
+// Preparar y ejecutar la consulta SQL
+$sql = "INSERT INTO registros (nombre, email, mensaje) VALUES ('$nombre', '$email', '$mensaje')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Nuevo registro creado con éxito";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// Cerrar la conexión
+$conn->close();
+?>
